a3e110a9e23104a232e62bdce83b2eb01f914655
ET <tarakanova_ekaterina@hotmail.com>
2023-11-11 11:40:12 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const leftNumber = ref(0)
+const rightNUmber = ref(0)
+</script>
+
+<template>
+  <section class="input-area">
+    <label class="input-area__input">
+      <span>Enter left number</span>
+      <input type="number" id="number1" v-model.number="leftNumber"/>
+    </label>
+
+      <label class="input-area__input">
+        <span>Enter right number</span>
+        <input type="number" id="number2" v-model.number="rightNUmber"/>
+    </label>
+  </section>
+</template>
+
+<style scoped>
+.input-area {
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+}
+
+.input-area__input {
+  display: flex;
+  column-gap: 25px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    federation({
+      name: "two-numbers",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.vue",
+      },
+      shared: ["vue"],
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+})
